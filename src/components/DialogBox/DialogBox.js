6a4e65a9c9b08749e71964fd78fe64c141a92819
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-08-03 22:56:29 -0700

    stumbling over dialog-box component. maybe should have it at App.js level instead of Task.js level

---
+++ src/components/DialogBox/DialogBox.js
@@ -0,0 +1,19 @@
+// Styles
+import './DialogBox.css'
+// Hooks
+
+const DialogBox = () => {
+  return (
+    <div className='dialog-box-wrapper'>
+
+      <div className='dialog-box'>
+      <p>Are you sure?</p>
+      <button>Yes</button>
+      <button>No</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default DialogBox
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 // Components
 import AddTime from "../AddTime/AddTime"
 import Button from '../Button/Button'
+import DialogBox from "../DialogBox/DialogBox"
 // Style
 import './Task.css'
 // Icons
@@ -32,6 +33,11 @@ const Task = (props) => {
   const [mainTimeIsGrandTotal, setMainTimeIsGrandTotal] = useState(true)
 
 
+  // TODO Dialog Box Pieces of State/description/buttons values
+  const [dialogBoxVisible, setDialogBoxVisible] = useState(false)
+  const [dialogMessage, setDialogMessage] = useState('')
+
+
   // TODO Window Resizing for tiem display
   // const [screenWidth, setScreenWidth] = useState()
 
@@ -44,6 +50,20 @@ const Task = (props) => {
 
 
   //! Helper Functions
+  //// TODO Shows confirmaiton dialogue, and sets DialogBox/confirmation window message.
+  const toggleDialogBox = (message) => {
+    if(!dialogBoxVisible) {
+      setDialogMessage(message)
+      setDialogBoxVisible(true)
+    } else {
+      setDialogBoxVisible(false)
+    }
+  }
+
+
+
+
+
   //// Converts ms into a a string with hrs, mins, secs
   const convertTime = (ms, format = 'long') => {
     // let seconds = Math.round(ms / 1000)
@@ -309,6 +329,15 @@ const Task = (props) => {
   //! Component
   return (
     <li className={`task ${timerRunning ? 'running' : ''}`}>
+
+      {/* //TODO Dialog/Confirmation Box */}
+      {/* <div className={`${dialogBoxVisible ? '' : 'hidden'}`}> */}
+      {/* <div className='relative full-size'>
+        <DialogBox
+          toggleDialogBox={toggleDialogBox}
+        />
+      </div> */}
+
 
       {/*//// Task Top Row: Task Name, Setting Buttons  */}
       <div className='task-name text-shadow'>
