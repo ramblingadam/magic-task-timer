f2fd6573622956a0a3137cbbecfa7285596f53f7
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-08-24 01:26:04 -0500

    resized tutorial gifs, 50% total reduction

--- src/components/App.js
+++ src/components/App.js
@@ -178,7 +178,7 @@ const App = () => {
        
       </div>
 
-      <Footer />
+      {/* <Footer /> */}
 
     </div>
   )
--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -5,7 +5,7 @@ import './Help.css'
 import { useState } from 'react'
 
 // Images
-import slide01 from './slides/slide-01.png'
+import slide01 from './slides/slide-01.gif'
 import slide02 from './slides/slide-02.gif'
 import slide03 from './slides/slide-03.gif'
 import slide04 from './slides/slide-04.gif'
@@ -22,6 +22,8 @@ import slide14 from './slides/slide-14.gif'
 import slide15 from './slides/slide-15.gif'
 import slide16 from './slides/slide-16.gif'
 
+// import slide02v from './slides/slide-02.mp4'
+
 
 const Help = (props) => {
 
@@ -32,82 +34,98 @@ const Help = (props) => {
     {
       header: 'Welcome to Magic Task Timer!',
       desc: 'Take control of how you spend your time by becoming aware of how you spend it.\nMagic Task Timer is your own personal timeclock.\nTrack the time you spend on projects, tasks, freelancing, getting fit, needlework, chores, socializing, breakdancing, dragon slaying- whatever you want.\nPlease take a moment to review the many incredible features of Magic Task Timer- you won\'t regret it, I guarantee it.*\n*lack of regret not guaranteed. void where prohibited.',
-      image: slide01
+      image: slide01,
+      video: '',
     },
     {
       header: 'Add A Task',
       desc: 'This is it. This is your story.\nIt all begins now.\nClick the plus button in the header to add a task.\n',
-      image: slide02
+      image: slide02,
+      // video: slide02v,
     },
     {
       header: 'Play/Pause Task Timer',
       desc: 'Click a task\'s play button to start the timer for that task. You may have any number of task timers running simultaneously.\nClick it again to stop the timer, which will save the elapsed time to your task for today.\nIf you close or suspend the browser tab while a timer is running, the currently running interval will NOT be saved. Please remember to stop all running timers before closing or suspending the browser tab to ensure you do not lose any time.',
-      image: slide04
+      image: slide04,
+      video: '',
     },
     {
       header: 'Sort Your Tasks',
       desc: 'Click the arrows on the left of a task to sort it up or down.',
-      image: slide03
+      image: slide03,
+      video: '',
     },
     {
       header: 'Toggle Timer Timeframe',
       desc: 'Click the task timer display to toggle the between \'All Time\' and \'Today\'.',
-      image: slide05
+      image: slide05,
+      video: '',
     },
     {
       header: 'View Task History',
       desc: 'The clock icon opens the History pane.\n Hover over a date on the heatmap to see the time spent on your task on that date.\n Click on the History button below the heatmap to reveal the history table.',
-      image: slide06
+      image: slide06,
+      video: '',
     },
     {
       header: 'Woops.',
       desc: 'Forgot you had the timer running, or didn\'t have access to the app while performing a task?\nNo problem!\n You can manually adjust the time spent on a task for any particular day.\nLucky you.',
-      image: slide07
+      image: slide07,
+      video: '',
     },
     {
       header: 'Edit Task History',
       desc: `Select a date by entering it manually or by selecting it on either the heatmap or the history table.\nEnter a number of hours and/or minutes into the form.\nThen, use the big friendly buttons to add to, subtract from, overwrite, or completely delete the logged time for that date.\n`,
-      image: slide08
+      image: slide08,
+      video: '',
     },
     {
       header: 'Close History View',
       desc: 'You can close the History pane by clicking the clock icon again.',
-      image: slide09
+      image: slide09,
+      video: '',
     },
     {
       header: 'Edit Task Name',
       desc: 'The pencil icon allows you to change the name of a task.',
-      image: slide10
+      image: slide10,
+      video: '',
     },
     {
       header: 'Categorize A Task',
       desc: 'Organize your tasks with Categories!\nThe folder icon allows you to assign a task to a category.\nA tab will appear along the top of your tasks list for each category.\nClick on a tab to show the tasks for that category.\nA task\'s category can be changed at any time.',
-      image: slide11
+      image: slide11,
+      video: '',
     },
     {
       header: 'Delete Task',
       desc: 'Click the trash icon to permanently delete a task and all time logged for it.\nTo delete ALL of your data forever and ever, clear your browser cache.\nSometimes, it\'s just not meant to be.\nC\'est la vie.\n',
-      image: slide16
+      image: slide16,
+      video: '',
     },
     {
       header: 'Dividers',
       desc: 'You can also add horizontal dividers into your task lists by creating a task with the name \'divider\'.\nDividers can be sorted and deleted just like tasks.\nFor that extra exquisite extra li\'l bit of organization to keep your many projects nice and tidy.\nGive it a try!',
-      image: slide12
+      image: slide12,
+      video: '',
     },
     {
       header: 'Settings',
       desc: 'Click the Settings button to personalize your Magic Task Timer experience with theming and other exciting options.*\n\n\n\n*options may not be exciting',
-      image: slide13
+      image: slide13,
+      video: '',
     },
     {
       header: 'Help',
       desc: 'Fear not, braave task warrior!\nIf you ever forget anything, you can view this tutorial at any time by clicking the Help button.',
-      image: slide14
+      image: slide14,
+      video: '',
     },
     {
       header: 'Enjoy!',
       desc: 'I hope you find this app useful!\nIf so, share it with your friends, and reach out with feedback via Twitter or Github!\n(Links can be found in the footer)',
-      image: slide15
+      image: slide15,
+      video: '',
     },
   ]
 
@@ -141,6 +159,7 @@ const Help = (props) => {
             
             <div className='slide-img-wrapper'>
               <img className='slide-img' src={slides[currentSlide].image} alt="" onClick={() => navigateSlides('next')}/>
+              {/* <video src={slides[currentSlide].video} autoPlay loop></video> */}
             </div>
 
             <div className='slide-desc' onClick={() => navigateSlides('next')}>
@@ -194,6 +213,13 @@ const Help = (props) => {
           {/* <p className='btn btn-history btn-skip' onClick={props.toggleHelpOpen}>Skip</p> */}
         </div>
       {/* </ul> */}
+      <div className='img-preloader'>
+        {slides.map(slide => (
+          <div>
+            <img src={slide.image} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
