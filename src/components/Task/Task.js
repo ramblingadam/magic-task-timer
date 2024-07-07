90b1309c36eb1a9b2d0a5525c73941dd5513ee27
Adam <96756923+ramblingadam@users.noreply.github.com>
2024-07-07 01:31:25 -0500

    fix task deletion

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -255,7 +255,7 @@ const Task = (props) => {
             <p className='warning'>Be careful! This cannot be undone.</p>
           </>
         ),
-        onAccept: () => deleteTask,
+        onAccept: deleteTask,
         yesText: 'Delete Task',
         isDestructiveAction: true,
       })
